564e4d0577dbc34a1e3df1ca215d37878223f051
Jahangir Iqbal <jiqbal@vibrenthealth.com>
2019-03-25 16:50:13 -0400

    comments

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import reactDom from "react-dom";
 
+//components
 const Button = () => <button>Add!</button>;
-
 const Display = props => <p>Score:{props.score}</p>;
 
+// app
 const App = () => (
   <div>
     <Display score="10" />
